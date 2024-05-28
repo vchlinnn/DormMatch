480be8308537cfc613ab6db2972a31f3d187a11a
Simon <sstur@me.com>
2024-05-27 17:15:07 -0700

    Add Firebase

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,26 @@
-import { Home } from '@tamagui/lucide-icons';
+import { collection, getDocs } from 'firebase/firestore/lite';
 import { Button, YStack } from 'tamagui';
+
+import { db } from '../support/firebase';
+
+async function getPosts() {
+  const allPosts = await getDocs(collection(db, 'posts'));
+  return allPosts.docs.map((post) => ({ id: post.id, ...post.data() }));
+}
 
 export default function App() {
   return (
     <YStack flex={1} justifyContent="center" alignItems="center">
-      <Button icon={<Home />}>Hello world!</Button>
+      <Button
+        onPress={async () => {
+          const posts = await getPosts();
+          for (const post of posts) {
+            console.log('>>', post);
+          }
+        }}
+      >
+        Get Posts
+      </Button>
     </YStack>
   );
 }
